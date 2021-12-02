Fixed calculation bug
Fixed bug in calculation of income tax/insurance tax
Code Refactoring

main.py
@@ -16,77 +16,78 @@ import matplotlib.pyplot as plt
 CONSTANTS
 """
 
+weeks_per_month = 4.34524
+
 full_time_hours = 38.5
 
 # not taxed, 12 times a year
-monthly_benefits = 123.0
+monthly_benefits = 0.0
 
 # used for x-Axes
-max_x_salary = 6000
+max_x_salary = 7500
 
 # for manual calculation
-part_time_hours = 20.0
+target_part_time_hours = 16.0
 
 # for manual calculation
 # including monthly benefits
-target_monthly_net_salary = 1234
+target_monthly_net_salary = 1000
 
 # 2021
 # https://www.finanz.at/steuern/lohnsteuertabelle/
 income_tax_levels = np.array([[9999999999, 83349.33, 55],
-                            [83349.33, 7516, 50],
-                            [7516, 5016, 48],
-                            [5016, 2599.33, 42],
-                            [2599.33, 1516, 35],
-                            [1516, 1099.33, 20]])
+                              [83349.33, 7516, 50],
+                              [7516, 5016, 48],
+                              [5016, 2599.33, 42],
+                              [2599.33, 1516, 35],
+                              [1516, 1099.33, 20]])
 
 
 # Insurance tax does really only depend on thresholds
-def insurance_tax(gross_salary):
-    if gross_salary < 478.81:
+def insurance_tax(gross_salary_monthly):
+    if gross_salary_monthly < 478.81:
         insurance_tax_percent = 0.0
-    elif 478.81 <= gross_salary < 1790.0:
+    elif 478.81 <= gross_salary_monthly < 1790.0:
         insurance_tax_percent = 15.12
-    elif 1790.0 <= gross_salary < 1953.0:
+    elif 1790.0 <= gross_salary_monthly < 1953.0:
         insurance_tax_percent = 16.12
-    elif 1953.0 <= gross_salary < 2117.0:
+    elif 1953.0 <= gross_salary_monthly < 2117.0:
         insurance_tax_percent = 17.12
-    elif 2117.0 <= gross_salary < 5550:
+    elif 2117.0 <= gross_salary_monthly < 5550:
         insurance_tax_percent = 18.12
     # > 5500 Euro -> constant amount
     else:
         return 1005.66
 
-    return gross_salary * insurance_tax_percent / 100.0
+    return gross_salary_monthly * insurance_tax_percent / 100.0
 
 
-def holiday_insurance_tax(gross_salary):
-    if gross_salary < 475.86:
+def holiday_insurance_tax(gross_salary_monthly):
+    if gross_salary_monthly < 475.86:
         holiday_salary_tax_percent = 0.0
-    elif gross_salary < 1790.0:
+    elif gross_salary_monthly < 1790.0:
         holiday_salary_tax_percent = 14.12
-    elif gross_salary < 1953.0:
+    elif gross_salary_monthly < 1953.0:
         holiday_salary_tax_percent = 15.12
-    elif gross_salary < 2117.0:
+    elif gross_salary_monthly < 2117.0:
         holiday_salary_tax_percent = 16.12
-    elif gross_salary < 5550:
+    elif gross_salary_monthly < 5550:
         holiday_salary_tax_percent = 17.12
     # > 5500 Euro -> constant amount
     else:
         return 1900.32
 
-    return gross_salary * holiday_salary_tax_percent / 100.0
+    return gross_salary_monthly * holiday_salary_tax_percent / 100.0
 
 
 # Does not affect the whole gross income -> insurance taxes are not taken into account
 # Percentage only affects the amount over the certain threshold
 def income_tax(gross_salary_monthly):
-
     # insurance tax is tax free
-    gross_salary_annually = gross_salary_monthly * 14 - \
-                            12 * insurance_tax(gross_salary_monthly) - 2 * holiday_insurance_tax(gross_salary_monthly)
+    gross_salary_monthly = gross_salary_monthly * 14 - \
+                           12 * insurance_tax(gross_salary_monthly) - 2 * holiday_insurance_tax(gross_salary_monthly)
 
-    gross_salary_monthly_taxfree = gross_salary_annually / 14
+    gross_salary_monthly_taxfree = gross_salary_monthly / 14
 
     income_tax_monthly = 0
 
@@ -192,37 +193,37 @@ def holiday_income_tax(gross_salary_monthly):
     return holiday_salary_tax
 
 
-def calculate_annual_net_salary_and_taxes(gross_monthly_income):
+def calculate_annual_net_salary_and_taxes(gross_monthly_income_fulltime, working_hours):
+    gross_monthly_income = gross_monthly_income_fulltime * (working_hours / full_time_hours)
     gross_yearly_income = gross_monthly_income * 14
 
+    # income tax calculation
     income_tax_monthly = income_tax(gross_monthly_income)
-    holiday_income_tax_monthly = holiday_income_tax(gross_monthly_income) * 2
+    holiday_income_tax_monthly = holiday_income_tax(gross_monthly_income)
 
+    # insurance tax calculation
     insurance_tax_monthly = insurance_tax(gross_monthly_income)
     holiday_insurance_tax_monthly = holiday_insurance_tax(gross_monthly_income)
 
-    income_tax_sum = income_tax_monthly * 12 + holiday_income_tax_monthly * 2
+    # yearly taxes calculation
+    income_tax_yearly = income_tax_monthly * 12 + holiday_income_tax_monthly * 2
     insurance_tax_yearly = insurance_tax_monthly * 12 + holiday_insurance_tax_monthly * 2
 
-    net_yearly_income = gross_yearly_income - income_tax_sum - insurance_tax_yearly
+    # net income calculation
+    net_yearly_income = gross_yearly_income - income_tax_yearly - insurance_tax_yearly + 12 * monthly_benefits
+    net_monthly_income = net_yearly_income / 14.0
+    net_hourly_income = net_monthly_income / weeks_per_month / working_hours
 
-    return np.array([gross_yearly_income, income_tax_sum, insurance_tax_yearly, net_yearly_income])
-
-
-def calculate_net_hourly_salary_from_gross(gross_monthly_income):
-    net_salary_annually = calculate_annual_net_salary_and_taxes(gross_monthly_income)[3]
-    weeks_per_month = 4.34524
-
-    return net_salary_annually / 12.0 / weeks_per_month / full_time_hours
+    return np.array([gross_yearly_income, income_tax_yearly, insurance_tax_yearly,
+                     net_yearly_income, net_monthly_income, net_hourly_income])
 
 
 def main_func():
-
-    gross_salary = np.linspace(0, max_x_salary, 100)
-    statistics_matrix = np.zeros((gross_salary.size, 4))
+    gross_salary = np.linspace(0, max_x_salary, 400)
+    statistics_matrix = np.zeros((gross_salary.size, 6))
 
     for i in range(gross_salary.size):
-        statistics_matrix[i, :] = np.array(calculate_annual_net_salary_and_taxes(gross_salary[i]))
+        statistics_matrix[i, :] = np.array(calculate_annual_net_salary_and_taxes(gross_salary[i], full_time_hours))
 
     plt.figure(0)
     fig, axs = plt.subplots(2, 1)
@@ -240,8 +241,9 @@ def main_func():
 
     salary_vector = np.zeros(gross_salary.size)
 
+    # gross monthly income / net hourly salary
     for i in range(gross_salary.size):
-        salary_vector[i] = calculate_net_hourly_salary_from_gross(gross_salary[i])
+        salary_vector[i] = statistics_matrix[i, 5]
 
     axs[1].set_xlabel('Monthly gross income full-time [EUR/month]')
     axs[1].set_ylabel('Net income/hour [EUR/h]')
@@ -255,20 +257,24 @@ def main_func():
     fig2.set_dpi(250)
     ax = plt.axes(projection='3d')
 
-    hour_vector = np.linspace(0, full_time_hours * 1.5, gross_salary.size)
+    hour_vector = np.linspace(0.1, full_time_hours * 1.5, gross_salary.size)
     X, Y = np.meshgrid(gross_salary, hour_vector)
-
     Z = np.zeros((gross_salary.size, hour_vector.size))
+
+    optimal_salary_found = False
 
     for x in range(gross_salary.size):
         for y in range(hour_vector.size):
-            Z[x][y] = calculate_net_hourly_salary_from_gross(gross_salary[x]) * (hour_vector[y] * 4.34524)
+            # Monthly net income with the specified hours
+            Z[x][y] = calculate_annual_net_salary_and_taxes(gross_salary[x], hour_vector[y])[4]
 
-            if Z[x][y] + monthly_benefits > target_monthly_net_salary and hour_vector[y] <= part_time_hours:
+            if not optimal_salary_found and Z[x][y] > target_monthly_net_salary \
+                    and hour_vector[y] <= target_part_time_hours:
                 print('Gross salary for ' + str(target_monthly_net_salary) + ' net with '
-                      + str(part_time_hours) + ' hours: ' + str(gross_salary[x]))
+                      + str(hour_vector[y]) + 'hours: ' + str(gross_salary[x]))
+                optimal_salary_found = True
 
-    ax.set_xlabel('Monthly gross income full-time [Euro/month]')
+    ax.set_xlabel('Monthly gross income full-time')
     ax.set_ylabel('Working hours [h]')
     ax.set_zlabel('Monthly net income [Euro/month]')
 